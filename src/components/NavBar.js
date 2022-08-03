2ab22162034435bdf4a84ad6d411daa870a20a3e
mjaxe <makstin33@gmail.com>
2022-08-02 21:46:17 -0400

    first changes of stripping down the app to bare bones, and start adding in a menu at the top

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+            <a className="Left-title"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
