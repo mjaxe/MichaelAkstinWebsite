634214aa8252715f27f8055bdd2e525ac6969742
mjaxe <makstin33@gmail.com>
2022-08-09 18:10:18 -0400

    adding in the type writer of things I want it to read into the test

--- src/components/test.js
+++ src/components/test.js
@@ -7,57 +7,99 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './test.css'
 
-function Testcomponents () {
-    const Hello = (props) =>{
-        return(
-            <div>
-                <h1>Hello {props.name}, you are {props.age} years old</h1>
-            </div>
-        )
+function Testcomponents() {
+  var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    const Peter = {
-        name: 'peter',
-        age: 27
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
     }
 
-    const Linda = {
-        name: 'linda',
-        age: 29
-    }
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
 
-    return(
-        <div>
-        <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="http://google.com">Google</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div class="typewriter">
-    <h1 class="line-1 anim-typewriter">Animation typewriter style using css steps()</h1>
+  window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Michael Akstin</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="http://google.com">Google</Nav.Link>
+              <NavDropdown title="Academia" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Course Work</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Greek Life
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Athletics</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
+      <h1> 
+        <span
+          class="txt-rotate"
+          data-period="2000"
+          data-rotate='[ "My name is Mike.", "I am a computer science student.", "I am a fraternity brother.", "I am a web developer.", "I am a ski racer." ]'></span>
+      </h1>
     </div>
-    </div>
-    
-    )
-  }
-  
-  export default Testcomponents
-  
+
+  )
+}
+
+export default Testcomponents
