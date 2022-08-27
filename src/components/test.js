47de6e91677b49aec6cda0cdb8e736c1e97e7b9e
mjaxe <makstin33@gmail.com>
2022-08-26 20:10:42 -0400

    added a new card style that is linked to my github

--- src/components/test.js
+++ src/components/test.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
+import './test.css'
 
 const Testcomponents = () => {
   return (
-    <div>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/About">About</Link>
-    </li>
-  </div>
+    <div class="container">
+      <div class="square">
+        <img className='card1img' src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80" class="mask" />
+     <div class="h1">Is Apple a Design Company?</div>
+        <p>Apple is more than a tech company; it became a culture unto itself, a passion of most of people and the birthplace of the world’s most revolutionized products.</p>
+        
+     <div><a href="https://github.com/mjaxe/michaelakstinwebsite" target="_" class="button">Read More</a></div>
+      </div>
+      
+      
+      
+      </div>
+      
+        
+      
+      
+    
   )
 }
 
